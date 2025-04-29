745a87a82f5b6fc021f168f6f132b8a6ca46d261
doomguyoncoke <doomguyoncoke@gmail.com>
2025-04-29 17:31:40 +0200

    creating nvim remote repository

---
+++ lua/config/set.lua
@@ -0,0 +1,25 @@
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set expandtab")
+
+vim.cmd("set smartindent")
+
+vim.cmd("set nowrap")
+
+vim.cmd("set nu")
+vim.cmd("set relativenumber")
+
+vim.cmd("set colorcolumn=80")
+
+vim.cmd("set nohlsearch")
+vim.cmd("set incsearch")
+
+vim.cmd("set termguicolors")
+
+vim.cmd("set scrolloff=8")
+vim.cmd("set signcolumn")
+
+vim.cmd("set updatetime=50")
+
+vim.g.mapleader = " "
